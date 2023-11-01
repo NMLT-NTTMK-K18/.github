4567085279cd9c597034ced8c91e52c6cd3b2b2e
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2023-11-01 04:13:20 +0000

    🐛 cp command

--- scripts/githubPageSetup.sh
+++ scripts/githubPageSetup.sh
@@ -4,7 +4,8 @@ USER_GITHUB_TOKEN=$1
 
 git clone https://$USER_GITHUB_TOKEN@github.com/NMLT-NTTMK-K18/nmlt-nttmk-k18.github.io Github-Page-Repo --depth 1
 
-rm -rf ./Github-Page-Repo/docs/
+rm -rf ./Github-Page-Repo/docs/*
 
-cp profile/README.md Github-Page-Repo/docs/
-cp -r ./img/ ./Github-Page-Repo/docs/
+cp profile/README.md Github-Page-Repo/docs/README.md
+mkdir Github-Page-Repo/docs/img
+cp -r ./img/ ./Github-Page-Repo/docs/img/
