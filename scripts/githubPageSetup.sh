3e559651d6d9faefeccbf663469aee604b7d3d4b
KevinNitroG <trannguyenthaibinh46@gmail.com>
2023-11-01 13:32:30 +0700

    🔨 No docs folder

--- scripts/githubPageSetup.sh
+++ scripts/githubPageSetup.sh
@@ -4,16 +4,12 @@ USER_GITHUB_TOKEN=$1
 
 git clone https://$USER_GITHUB_TOKEN@github.com/NMLT-NTTMK-K18/nmlt-nttmk-k18.github.io Github-Page-Repo --depth 1
 
-folder_path="path/to/your/folder"
-
-if [ ! -d "./Github-Page-Repo/docs" ]; then
-    mkdir -p "./Github-Page-Repo/docs"
-    echo "Folder Github-Page-Repo/docs has been created."
-elif [ -d "./Github-Page-Repo/docs/img" ]; then
-    rm -rf ./Github-Page-Repo/docs/img/
-    mkdir Github-Page-Repo/docs/img
+if [ -d "./Github-Page-Repo/img" ]; then
+    rm -rf ./Github-Page-Repo/img/
+    mkdir Github-Page-Repo/img
     echo "Re-create img folders"
 fi
 
-cp -f profile/README.md Github-Page-Repo/docs/README.md
-cp -r ./img/ ./Github-Page-Repo/docs/
+cp -f profile/README.md Github-Page-Repo/README.md
+sed -i 's/\.\.\/\.\.\/\.\.\//https:\/\/github.com\/NMLT-NTTMK-K18\//g' Github-Page-Repo/README.md
+cp -r ./img/ ./Github-Page-Repo/
